24c4852779142d8504af13ed83043d7571e5bda2
maxtuzz <maxtuzzolino@gmail.com>
2016-12-08 16:23:22 +1300

    Use new API endpoints

--- src/providers/reward.service.ts
+++ src/providers/reward.service.ts
@@ -10,14 +10,14 @@ export class RewardService {
     constructor(public atpHttp: AtpHttp, public model: Model) {}
 
     getRewards(): Observable<Reward[]> {
-        return this.atpHttp.doGetBackground("/app/achievement/list");
+        return this.atpHttp.doGetBackground("/app/reward/list");
     }
 
     claimReward(type: string) {
-      this.atpHttp.doPost("/app/achievement/claim/" + type, {}, "claiming reward").subscribe(
+      this.atpHttp.doPost("/app/reward/claim/" + type, {}, "claiming reward").subscribe(
         data => {
           this.model.user = data.user;
-          this.model.setRewards(data.achievements);
+          this.model.setRewards(data.rewards);
         }
       );
     }
