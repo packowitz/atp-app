be756c13b29024d88458e03bad742c92ad99ee67
maxtuzz <maxtuzzolino@gmail.com>
2016-12-15 15:16:21 +1300

    Further styling changes for highscore page

--- src/pages/highscore/highscoreEntry.ts
+++ src/pages/highscore/highscoreEntry.ts
@@ -4,14 +4,18 @@ import {HighscoreUser} from "../../providers/domain/highscoreUser";
   selector: 'highscore-entry',
   template: `<ion-item *ngFor="let hs of highscore; let i = index" class="highscore-row" [class.itsme]="hs.itsme">
                 <ion-label class="highscore-entry">
-                    <span>{{i + 1}}.</span>
+                    <ion-note padding-left padding-right>
+                        {{i + 1}}
+                    </ion-note>
                     <span class="highscore-name">
                         <img src="assets/img/flags/{{hs.country}}.png" class="flag highscore-name-element">
                         <ion-icon [name]="hs.male ? 'male' : 'female'" class="highscore-name-element"></ion-icon>
                         <span class="highscore-name-element">{{getAge(hs)}} yrs.</span>
                         <span class="highscore-name-element">{{hs.username? hs.username : 'Anonymous'}}</span>
                     </span>
-                    <span>{{showWeek ? hs.surveysAnsweredWeek : hs.surveysAnswered}} <ion-icon name="trophy" class="trophy"></ion-icon></span>
+                    <ion-badge color="white">
+                    {{showWeek ? hs.surveysAnsweredWeek : hs.surveysAnswered}} <ion-icon name="trophy" color="atp-golder"></ion-icon>
+                    </ion-badge>
                 </ion-label>
             </ion-item>`
 })
