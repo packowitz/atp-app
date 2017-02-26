656a1fc7b756298519aa0dc8f4403a027ea557b4
maxtuzz <maxtuzzolino@gmail.com>
2017-02-26 19:12:26 +1300

    Added connectivity service layer

---
+++ src/providers/services/connectivity.service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from "@angular/core";
+import {Platform} from "ionic-angular";
+import {Network} from "ionic-native";
+
+/**
+ * Detects if internet connection is available
+ * @author: Max Tuzzoilino
+ */
+@Injectable()
+export class ConnectivityService {
+  onDevice: boolean;
+
+  constructor(public platform: Platform) {
+    this.onDevice = this.platform.is('cordova');
+  }
+
+  isOnline(): boolean {
+    if (this.onDevice && Network.type != 'none') {
+      return Network.type != 'none';
+    } else {
+      return navigator.onLine;
+    }
+  }
+
+  isOffline(): boolean {
+    if (this.onDevice && Network.type != 'none') {
+      return Network.type != 'none';
+    } else {
+      return !navigator.onLine;
+    }
+  }
+}
