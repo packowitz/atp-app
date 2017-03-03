66a9fd1d7465cbbb2dccf283b061b3f2176acf6b
packowitz <PhilippSchum@web.de>
2017-03-03 14:05:10 +1300

    redesign about page

--- src/pages/about/about.component.ts
+++ src/pages/about/about.component.ts
@@ -1,18 +1,10 @@
 import {Component} from "@angular/core";
 import {ViewController, Platform} from "ionic-angular";
 
-/**
- * About page
- * Author: Max Tuzzolino
- */
-
 @Component({
   templateUrl: 'about.component.html'
 })
 export class AboutComponent {
-
-  aboutExpand: boolean = false;
-  legalExpand: boolean = false;
 
   constructor(public viewCtrl: ViewController,
               public platform: Platform) {
